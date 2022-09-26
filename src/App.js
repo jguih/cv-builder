5dab141ac307da1ed5bab4377b2e2e2ecdea62b5
Jose Guilherme <gui.kcoal+github@hotmail.com>
2022-09-26 10:51:12 -0300

    Add cv form and cv preview components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Form, FormLabel, FormGroup, FormControl, Row, Col, Container, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+  }
+
+  render() {
+    return (
+      <Container fluid className="cv-container">
+        <Row>
+          <Col lg="6" className="cv-form">
+            <Form>
+              <Row>
+                <Col>
+                  <FormGroup className='mb-3'>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl type='text' placeholder='Your First Name'></FormControl>
+                  </FormGroup>
+                </Col>
+                <Col> 
+                  <FormGroup className='mb-3'>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl type='text' placeholder='Your Last Name'></FormControl>
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+          <Col lg="6" className="cv-preview">
+            <Alert variant='success'>
+              Your CV
+              <hr></hr>
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+
+    );
+  }
 }
 
 export default App;
